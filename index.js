c41b9536900eca39b3996c4a617a8dce50b56524
jasmin47 <alxsanborn@gmail.com>
2016-08-31 13:30:34 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kittenAdded = [...kittens, name]
+  return kittenAdded
+}
+
+function prependKitten(name) {
+  var kittenAdded = [name, ...kittens]
+  return kittenAdded
+}
+
+function removeLastKitten() {
+  var lastRemoved = [...kittens]
+  lastRemoved.pop()
+  return lastRemoved
+}
+
+function removeFirstKitten() {
+  var firstRemoved = [...kittens]
+  firstRemoved.shift()
+  return firstRemoved
+}
